a31a45911b360ec87fe0e54e1755c437a4a25ff3
Ritwik <sritwik03@gmail.com>
2021-10-17 19:46:29 +0530

    initial commit

---
+++ digit.py
@@ -0,0 +1,65 @@
+# Necessary Imports
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import scale
+
+# Reading data
+train_orig = pd.read_csv('mnist_train.csv')
+test_orig = pd.read_csv('mnist_test.csv')
+
+train = train_orig[:30000]
+test = test_orig[:5000]
+
+# Separating X and y variables
+y = train['label']
+X = train.drop(['label'], axis=1)
+
+# Normalizing and scaling data
+X = X/255.0                             # as 255 is max pixel value
+test = test/255.0
+X_scaled = scale(X)
+
+# Train - Test split
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, train_size=0.2, random_state=10)
+
+
+# # Logistic Regressor
+# clf = LogisticRegression()
+# clf.fit(X_train, y_train)
+# pred = clf.predict(X_test)
+# print("Accuracy:", accuracy_score(y_true=y_test, y_pred=pred), "\n")
+
+
+# # # Decision tree
+# clf_tree = DecisionTreeClassifier()
+# clf_tree.fit(X_train, y_train)
+# pred_tree = clf_tree.predict(X_test)
+# print("Accuracy:", accuracy_score(y_true=y_test, y_pred=pred_tree), "\n")
+
+
+# # Linear Support Vector Machine
+# clf_svm = SVC(kernel='linear')
+# clf_svm.fit(X_train, y_train)
+# pred_svm_lin = clf_svm.predict(X_test)
+# print("Accuracy:", accuracy_score(y_true=y_test, y_pred=pred_svm_lin), "\n")
+
+
+# # Non-Linear Support Vector Machine
+clf_svm = SVC(kernel='rbf', C=10, gamma=0.001)
+clf_svm.fit(X_train, y_train)
+pred_svm_nlin = clf_svm.predict(X_test)
+print("Accuracy:", accuracy_score(y_true=y_test, y_pred=pred_svm_nlin), "\n")
+
+# Plot 6 random images from the test set and check prediction
+for i in (np.random.randint(0,1000,6)):
+    two_d = (np.reshape(X_test[i], (28, 28)) * 255)
+    plt.title('predicted label: {0}'. format(pred_svm_nlin[i]))
+    plt.imshow(two_d, cmap='gray')
+    plt.show()
